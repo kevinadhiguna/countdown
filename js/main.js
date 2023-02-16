b73aba529225e9e1f64fd6f24054c592c41c9059
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-16 10:26:34 +0700

    chore: format code

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 const countDate = new Date('Dec 31, 2023 23:59:59').getTime();
 
 function newYear() {
-    const now = new Date().getTime();
-    const gap = countDate - now;
+  const now = new Date().getTime();
+  const gap = countDate - now;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const d = Math.floor(gap / (day));
-    const h = Math.floor((gap % (day)) / (hour));
-    const m = Math.floor((gap % (hour)) / (minute));
-    const s = Math.floor((gap % (minute)) / second);
+  const d = Math.floor(gap / (day));
+  const h = Math.floor((gap % (day)) / (hour));
+  const m = Math.floor((gap % (hour)) / (minute));
+  const s = Math.floor((gap % (minute)) / second);
 
-    document.getElementById('day').innerText = d;
-    document.getElementById('hour').innerText = h;
-    document.getElementById('minute').innerText = m;
-    document.getElementById('second').innerText = s;
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText = s;
 }
 
-setInterval(function(){
-    newYear();
+setInterval(function() {
+  newYear();
 }, 1000);
